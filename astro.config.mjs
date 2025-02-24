b22f056661c50b0796c958b52479d59bfa6e769e
Andrew K <andrew.katsewich@gmail.com>
2025-02-24 23:19:06 +0300

    Update Tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
 import partytown from '@astrojs/partytown';
 import solidJs from '@astrojs/solid-js';
-import tailwind from '@astrojs/tailwind';
 import vtbot from 'astro-vtbot';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		svg: true,
+  experimental: {
+      svg: true,
 	},
-	//   output: 'server',
-	integrations: [solidJs(), vtbot(), tailwind(), partytown()],
+
+  //   output: 'server',
+  integrations: [solidJs(), vtbot(), partytown()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
