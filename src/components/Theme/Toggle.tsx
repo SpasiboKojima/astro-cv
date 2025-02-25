07953951049def1d0caf3f643477020dd4f271d1
Andrew K <andrew.katsewich@gmail.com>
2025-02-25 20:13:00 +0300

    Update theme toggler

--- src/components/Theme/Toggle.tsx
+++ src/components/Theme/Toggle.tsx
@@ -15,28 +15,22 @@ const ThemeToggle = () => {
 	});
 
 	return (
-		<>
-		<label class="swap absolute right-4 top-8 z-20 mx-auto -mt-4 origin-top-right p-4 transition-transform hover:scale-150 lg:top-0">
+		<label class="swap absolute top-8 right-4 z-20 mx-auto -mt-4 origin-top-right p-4 transition-transform hover:scale-150 lg:top-0">
 			<input name="username" type="checkbox" checked={checked()} onChange={handleToggle} />
 
 			<div class="swap-on size-5 border-[7px] border-white bg-black" />
 
 			<div class="swap-off size-5 border-[7px] border-black bg-white" />
 		</label>
-
-		{/* <input name="username" type="checkbox" checked={checked()} onChange={handleToggle} /> */}
-
-		</>
 	);
 };
 
 const ThemeToggleWrapper = () => {
 	return (
 		<ThemeProvider>
-			
 			<ThemeToggle />
 		</ThemeProvider>
-);
+	);
 };
 
 export { ThemeToggleWrapper as ThemeToggle };
