c33b939e201bf14b76a9cdc5b349555c32f0a362
Andrew K <andrew.katsewich@gmail.com>
2025-03-30 22:27:31 +0300

    Update image loading

--- src/components/ProjectImages.tsx
+++ src/components/ProjectImages.tsx
@@ -16,7 +16,7 @@ const Picture = (props: PictureProps) => {
 	return (
 		<picture>
 			<For each={options().options} fallback={null}>
-				{(item, index) => <source sizes={item.sizes} srcset={options().srcSet[index()].attribute} type={`image/${item.format}`} />}
+				{(item, index) => <source sizes={props.sizes} srcset={options().srcSet[index()].attribute} type={`image/${item.format}`} />}
 			</For>
 			<img {...props} alt={props.alt} src={isImageMetadata(src) ? src.src : src} />
 		</picture>
@@ -72,7 +72,7 @@ export default function ProjectImages(props: ProjectImagesProps) {
 								width={item.attributes.width}
 								height={item.attributes.height}
 								decoding={item.attributes.decoding}
-								loading={item.attributes.loading}
+								loading={index() === 0 ? 'eager' : item.attributes.loading}
 								sizes="(max-width: 48rem) 100vw, 21.25rem"
 							/>
 						</div>
@@ -83,10 +83,23 @@ export default function ProjectImages(props: ProjectImagesProps) {
 			<dialog class="modal" ref={modalRef}>
 				<div class="modal-box w-auto max-w-full p-0 md:max-w-10/12">
 					<Show when={selectedImage()} fallback={<div>Loading...</div>}>
-						{(item) => <Picture options={item()} alt="Project's UI mockup" class="size-full max-h-screen object-contain" />}
+						{(item) => (
+							<Picture
+								options={{
+									...item(),
+									srcSet: item().src.map((src) => ({
+										attribute: src,
+										values: [],
+									})),
+								}}
+								sizes="auto"
+								alt="Project's UI mockup"
+								class="size-full max-h-screen object-contain"
+							/>
+						)}
 					</Show>
 				</div>
-				<form method="dialog" class="modal-backdrop">
+				<form method="dialog" class="modal-backdrop z-0">
 					<button>close</button>
 				</form>
 			</dialog>
