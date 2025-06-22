331b65dcecb3a55898096abfef8f702c8eebc7e8
Andrew K <andrew.katsewich@gmail.com>
2025-06-22 09:26:59 +0300

    Bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		svg: true,
-	},
-
 	//   output: 'server',
 	integrations: [solidJs(), vtbot()],
 
