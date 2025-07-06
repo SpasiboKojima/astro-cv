335e0d6572c1efb6ffe4a270482c40035f46498b
Andrew K <andrew.katsewich@gmail.com>
2025-07-06 11:44:42 +0300

    Update deps and version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import solidJs from '@astrojs/solid-js';
-import vtbot from 'astro-vtbot';
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	//   output: 'server',
-	integrations: [solidJs(), vtbot()],
+	integrations: [solidJs()],
 
 	vite: {
 		plugins: [tailwindcss()],
