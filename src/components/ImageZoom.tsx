8d186846528b811e85bf266229d3cd8f1b02d71a
Andrew K <andrew.katsewich@gmail.com>
2025-08-19 20:47:06 +0300

    Add image zoom WIP

---
+++ src/components/ImageZoom.tsx
@@ -0,0 +1,136 @@
+import mediumZoom, { type Zoom } from '@spasibokojima/medium-zoom';
+import type { JSX } from 'solid-js';
+import { createSignal, For, onMount, Show, splitProps } from 'solid-js';
+import type { ImageOpts } from '~/lib/image';
+
+interface PictureProps extends JSX.ImgHTMLAttributes<HTMLImageElement> {
+	options: ImageOpts;
+}
+
+const Picture = (props: PictureProps) => {
+	const [localProps, imageProps] = splitProps(props, ['options', 'sizes']);
+	const options = () => localProps.options;
+
+	const getSrc = () => {
+		const src = options().origSrc;
+		return typeof src === 'string' ? src : src.src;
+	};
+
+	return (
+		<picture>
+			<For each={options().srcSet} fallback={null}>
+				{(item) => <source sizes={localProps.sizes} srcset={item.src} type={`image/${item.format}`} />}
+			</For>
+			<img {...imageProps} alt={props.alt} src={getSrc()} />
+		</picture>
+	);
+};
+
+export const [loadedImages, setLoadedImages] = createSignal<Record<string, boolean>>({});
+
+// let zoomRef: Zoom;
+// export function getZoom() {
+// 	if (!zoomRef) {
+// 		zoomRef = mediumZoom({ background: 'rgba(0, 0, 0, 0.75)' });
+// 	}
+
+// 	return zoomRef;
+// }
+
+type ImageZoomProps = JSX.ImgHTMLAttributes<HTMLImageElement> & {
+	// zoomOptions?: ZoomOptions;
+	options: ImageOpts;
+
+	class: JSX.HTMLAttributes<'button'>['class'];
+	onClick?: () => void;
+	ref?: (el: HTMLImageElement | undefined) => void;
+	id: string;
+};
+
+export function ImageZoom(props: ImageZoomProps) {
+	const [localProps, imageProps] = splitProps(props, ['class', 'onClick', 'ref', 'aria-label']);
+
+	let imgRef: HTMLImageElement | undefined;
+	const isLoaded = () => loadedImages()[props.id];
+
+	const [isLoading, setIsLoading] = createSignal(false);
+
+	let zoomRef: Zoom;
+	function getZoom() {
+		if (!zoomRef) {
+			zoomRef = mediumZoom({ background: 'rgba(0, 0, 0, 0.75)' });
+		}
+
+		return zoomRef;
+	}
+
+	const attachZoom: (el: HTMLImageElement) => void = (node) => {
+		const zoom = getZoom();
+		if (!zoom) return;
+
+		if (node) {
+			zoom.attach(node);
+		} else {
+			zoom.detach();
+		}
+	};
+
+	onMount(() => {
+		attachZoom(imgRef!);
+	});
+
+	return (
+		<button
+			aria-label={localProps['aria-label']}
+			onClick={(e) => {
+				// e.preventDefault();
+				// e.stopPropagation();
+				// e.stopImmediatePropagation();
+
+				if (!imgRef) return;
+
+				localProps.onClick?.();
+				const hdSource = imageProps.options.src[imgRef.currentSrc.endsWith('avif') ? 0 : 1];
+
+				// imgRef.setAttribute('data-zoom-src', hdSource);
+				// return;
+
+				console.log('isLoaded() :>> ', isLoaded());
+				if (isLoaded()) {
+					imgRef.setAttribute('data-zoom-src', hdSource);
+					return getZoom().open();
+					// return getZoom().open({ target: imgRef });
+				}
+
+				setIsLoading(true);
+
+				const image = new Image();
+				image.src = hdSource;
+				image.onload = () => {
+					setIsLoading(false);
+					setLoadedImages({ ...loadedImages(), [props.id]: true });
+
+					imgRef?.setAttribute('data-zoom-src', hdSource);
+
+					zoomRef.open();
+					// zoomRef.open({ target: imgRef });
+				};
+			}}
+			class={localProps.class}
+		>
+			<Picture
+				{...imageProps}
+				ref={imgRef}
+				// ref={(el) => {
+				// 	imgRef = el;
+				// 	props.ref?.(el);
+				// }}
+			/>
+			<Show when={isLoading()}>
+				<div class="animate-[fade-in_.3s_forwards_.5s] opacity-0 flex-center fixed inset-0 z-[999999] size-full bg-black/40">
+					<span class="loading loading-ring w-12" />
+				</div>
+			</Show>
+		</button>
+	);
+}
--- src/components/ProjectImages.tsx
+++ src/components/ProjectImages.tsx
@@ -1,49 +1,50 @@
-import { createSignal, For, onMount, Show, splitProps, type JSX, type JSXElement } from 'solid-js';
+import { createSignal, For, onMount, type JSXElement } from 'solid-js';
 import type { ImageOpts } from '~/lib/image';
+import { ImageZoom, setLoadedImages } from './ImageZoom';
 
-interface PictureProps extends JSX.ImgHTMLAttributes<HTMLImageElement> {
-	options: ImageOpts;
-}
+// interface PictureProps extends JSX.ImgHTMLAttributes<HTMLImageElement> {
+// 	options: ImageOpts;
+// }
 
-const Picture = (props: PictureProps) => {
-	const [localProps, imageProps] = splitProps(props, ['options', 'sizes']);
-	const options = () => localProps.options;
+// const Picture = (props: PictureProps) => {
+// 	const [localProps, imageProps] = splitProps(props, ['options', 'sizes']);
+// 	const options = () => localProps.options;
 
-	const getSrc = () => {
-		const src = options().origSrc;
-		return typeof src === 'string' ? src : src.src;
-	};
+// 	const getSrc = () => {
+// 		const src = options().origSrc;
+// 		return typeof src === 'string' ? src : src.src;
+// 	};
 
-	return (
-		<picture>
-			<For each={options().srcSet} fallback={null}>
-				{(item) => <source sizes={localProps.sizes} srcset={item.src} type={`image/${item.format}`} />}
-			</For>
-			<img {...imageProps} alt={props.alt} src={getSrc()} />
-		</picture>
-	);
-};
+// 	return (
+// 		<picture>
+// 			<For each={options().srcSet} fallback={null}>
+// 				{(item) => <source sizes={localProps.sizes} srcset={item.src} type={`image/${item.format}`} />}
+// 			</For>
+// 			<img {...imageProps} alt={props.alt} src={getSrc()} />
+// 		</picture>
+// 	);
+// };
 
-const [loadedImages, setLoadedImages] = createSignal<Record<string, boolean>>({});
+// const [loadedImages, setLoadedImages] = createSignal<Record<string, boolean>>({});
 
-interface PictureWithLoadingProps extends PictureProps {
-	id: string;
-}
+// interface PictureWithLoadingProps extends PictureProps {
+// 	id: string;
+// }
 
-const PictureWithLoading = (props: PictureWithLoadingProps) => {
-	const isLoaded = () => loadedImages()[props.id];
+// const PictureWithLoading = (props: PictureWithLoadingProps) => {
+// 	const isLoaded = () => loadedImages()[props.id];
 
-	return (
-		<>
-			<Picture {...props} onload={() => setLoadedImages({ ...loadedImages(), [props.id]: true })} />
-			<Show when={!isLoaded()}>
-				<div class="animate-fade-in flex-center fixed inset-0 z-20 size-full bg-black/40">
-					<span class="loading loading-ring w-12" />
-				</div>
-			</Show>
-		</>
-	);
-};
+// 	return (
+// 		<>
+// 			<Picture {...props} onload={() => setLoadedImages({ ...loadedImages(), [props.id]: true })} />
+// 			<Show when={!isLoaded()}>
+// 				<div class="animate-fade-in flex-center fixed inset-0 z-20 size-full bg-black/40">
+// 					<span class="loading loading-ring w-12" />
+// 				</div>
+// 			</Show>
+// 		</>
+// 	);
+// };
 
 interface ProjectImagesProps {
 	images: ImageOpts[];
@@ -51,64 +52,92 @@ interface ProjectImagesProps {
 }
 
 export default function ProjectImages(props: ProjectImagesProps) {
-	let modalRef: HTMLDialogElement | undefined;
 	const [selectedIndex, setSelectedIndex] = createSignal<number | null>(null);
+
+	const imgRefs: (HTMLImageElement | undefined)[] = [];
 
 	onMount(() => {
 		setLoadedImages({});
+
+		// const zoom = getZoom();
+		// if (!zoom) return;
+
+		// if (imgRefs) {
+		// 	zoom.attach(imgRefs.filter((el) => el !== undefined) as HTMLElement[]);
+		// } else {
+		// 	zoom.detach();
+		// }
 	});
 
-	const selectedImage = () => {
-		const index = selectedIndex();
-		return index !== null ? props.images[index] : null;
-	};
+	// const selectedImage = () => {
+	// 	const index = selectedIndex();
+	// 	return index !== null ? props.images[index] : null;
+	// };
 
 	function changeImage(e: KeyboardEvent) {
-		if (e.key === 'ArrowRight') {
-			const index = selectedIndex();
-			if (modalRef?.open && index !== null && index < props.images.length - 1) {
-				setSelectedIndex(index + 1);
-			}
-		}
-		if (e.key === 'ArrowLeft') {
-			const index = selectedIndex();
-			if (modalRef?.open && index !== null && index > 0) {
-				setSelectedIndex(index - 1);
-			}
-		}
+		// const zoomRef = getZoom();
+		// if (e.key === 'ArrowRight') {
+		// 	const index = selectedIndex();
+		// 	if (index !== null && index < props.images.length - 1) {
+		// 		setSelectedIndex(index + 1);
+		// 		zoomRef.close().then(() => {
+		// 			zoomRef.open({ target: imgRefs[index + 1] });
+		// 		});
+		// 	}
+		// }
+		// if (e.key === 'ArrowLeft') {
+		// 	const index = selectedIndex();
+		// 	if (index !== null && index > 0) {
+		// 		setSelectedIndex(index - 1);
+		// 		zoomRef.close().then(() => {
+		// 			zoomRef.open({ target: imgRefs[index - 1] });
+		// 		});
+		// 	}
+		// }
 	}
 
-	const selectImage = (index: number) => {
-		setSelectedIndex(index);
-		modalRef?.showModal();
-	};
+	// const selectImage = (index: number) => {
+	// 	setSelectedIndex(index);
+	// 	modalRef?.showModal();
+	// };
 
 	return (
 		<>
-			<div class="grid gap-4 md:grid-cols-2">
+			<div class="grid gap-4 md:grid-cols-2" onkeydown={changeImage}>
 				<For each={props.images} fallback={<div>Loading...</div>}>
 					{(item, index) => (
-						<button
+						<ImageZoom
 							aria-label={`Open image ${index() + 1} fullscreen`}
-							onClick={() => selectImage(index())}
-							class="self-center overflow-hidden rounded-xl border border-gray-200 shadow"
-						>
-							<Picture
-								options={item}
-								class="h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
-								alt="Project's UI mockup"
-								width={item.attributes.width}
-								height={item.attributes.height}
-								decoding={item.attributes.decoding}
-								loading={index() === 0 ? 'eager' : item.attributes.loading}
-								sizes="(max-width: 48rem) 100vw, 21.25rem"
-							/>
-						</button>
+							class="cursor-zoom-in scale-100 self-center overflow-hidden rounded-xl border border-gray-200 shadow focus:scale-106 hover:scale-106 transition-transform duration-300 ease-in-out"
+							id={index().toString()}
+							onClick={() => setSelectedIndex(index())}
+							ref={(el) => {
+								imgRefs[index()] = el;
+							}}
+							options={item}
+							alt="Project's UI mockup"
+							width={item.attributes.width}
+							height={item.attributes.height}
+							decoding={item.attributes.decoding}
+							loading={index() === 0 ? 'eager' : item.attributes.loading}
+							sizes="(max-width: 48rem) 100vw, 21.25rem"
+						/>
+
+						// {/* <Picture
+						// 	options={item}
+						// 	class="h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
+						// 	alt="Project's UI mockup"
+						// 	width={item.attributes.width}
+						// 	height={item.attributes.height}
+						// 	decoding={item.attributes.decoding}
+						// 	loading={index() === 0 ? 'eager' : item.attributes.loading}
+						// 	sizes="(max-width: 48rem) 100vw, 21.25rem"
+						// /> */}
 					)}
 				</For>
 			</div>
 
-			<dialog class="modal" ref={modalRef} onkeydown={changeImage}>
+			{/* <dialog class="modal" ref={modalRef} onkeydown={changeImage}>
 				<div class="modal-box w-auto max-w-full min-w-2/5 p-0 md:max-w-10/12">
 					<Show
 						when={selectedImage()}
@@ -131,7 +160,7 @@ export default function ProjectImages(props: ProjectImagesProps) {
 								width={item().attributes.width}
 								height={item().attributes.height}
 								alt="Project's UI mockup"
-								class="size-full max-h-screen object-contain"
+								class="size-full max-h-screen cursor-zoom-out object-contain"
 								onClick={() => modalRef?.close()}
 							/>
 						)}
@@ -140,7 +169,7 @@ export default function ProjectImages(props: ProjectImagesProps) {
 				<form method="dialog" class="modal-backdrop">
 					<button>close</button>
 				</form>
-			</dialog>
+			</dialog> */}
 		</>
 	);
 }
