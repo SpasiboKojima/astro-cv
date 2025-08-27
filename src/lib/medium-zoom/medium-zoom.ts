17865c097c5d47f960331408630a32444b1098b7
Andrew K <andrew.katsewich@gmail.com>
2025-08-27 20:53:42 +0300

    Try to fix Safari scaling

--- src/lib/medium-zoom/medium-zoom.ts
+++ src/lib/medium-zoom/medium-zoom.ts
@@ -206,7 +206,7 @@ const mediumZoom = (selector?: ZoomSelector | ZoomOptionsParams, options: ZoomOp
 		const scale = Math.min(scaleX, scaleY);
 		const translateX = (-left + (viewportWidth - width) / 2 + zoomOptions.margin + container.left) / scale;
 		const translateY = (-top + (viewportHeight - height) / 2 + zoomOptions.margin + container.top) / scale;
-		const transform = `scale(${scale}) translate(${translateX}px, ${translateY}px)`;
+		const transform = `scale(${scale}) translate(${translateX}px, ${translateY}px) translateZ(0)`;
 
 		active.zoomed.style.transform = transform;
 	};
